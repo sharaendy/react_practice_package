01ec9b7c72a16977ef720c116cd2143d28f798f9
andrey.sh <aash7133@gmail.com>
2022-08-09 22:23:46 +0700

    correction App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,79 @@
+import uniqueId from 'lodash/uniqueId';
 import React from 'react';
-import classNames from 'classnames'
 
-export default class App extends React.Component {
-  contructor(props) {
+// BEGIN (write your solution here)
+export default class Component extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      leftBtnToggled: false,
-      rigthBtnToggled: false,
+      count: 0,
+      logs: [],
     };
   }
-  
-  handleClick = () => {
-    this.setState((state) => {});
-  }
 
-  render() {}
+  addLog = () =>
+    this.setState(({ count, logs }) => ({
+      logs: [{ count, id: uniqueId() }, ...logs],
+    }));
+
+  deleteLog = (id) => (e) => {
+    e.preventDefault();
+    const { logs } = this.state;
+    const newLogs = logs.filter((item) => item.id !== id);
+    this.setState({ logs: newLogs });
+  };
+
+  increaseCount = () => {
+    this.setState(({ count }) => ({ count: (count += 1) }));
+    this.addLog();
+  };
+
+  decreaseCount = () => {
+    this.setState(({ count }) => ({ count: (count -= 1) }));
+    this.addLog();
+  };
+
+  renderItem = ({ count, id }) => {
+    return (
+      <button
+        key={id}
+        onClick={this.deleteLog(id)}
+        type="button"
+        className="list-group-item list-group-item-action"
+      >
+        {count}
+      </button>
+    );
+  };
+
+  render() {
+    const { logs } = this.state;
+    const list = (
+      <div className="list-group">
+        {logs.map((item) => this.renderItem(item))}
+      </div>
+    );
+
+    return (
+      <div>
+        <div className="btn-group font-monospace" role="group">
+          <button
+            onClick={this.increaseCount}
+            type="button"
+            className="btn btn-outline-success"
+          >
+            +
+          </button>
+          <button
+            onClick={this.decreaseCount}
+            type="button"
+            className="btn btn-outline-danger"
+          >
+            -
+          </button>
+        </div>
+        {logs.length !== 0 ? list : null}
+      </div>
+    );
+  }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
+import './styles.css';
 
 const mountNode = document.getElementById('root');
 const root = ReactDOM.createRoot(mountNode);
